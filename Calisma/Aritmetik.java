4c81943ae3e488948a30151713fc21dfa338b812
Özlem Çetinkaya <21010310061@ogrenci.bartin.edu.tr>
2021-12-12 00:21:02 +0300

    aritmetik
    calisma

--- Calisma/Aritmetik.java
+++ Calisma/Aritmetik.java
@@ -1,2 +1,19 @@
+package deneme3;
 
 public class Aritmetik {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int n1, n2, n3, n4;
+        int a=17;
+        int b=74;
+        
+        n1= a+b;
+        n2= a-b;
+        n3= a*b;
+        n4= a/b;
+        System.out.println(n1+" "+n2+" "+n3+" "+n4);
+        
+	}
+
+}
