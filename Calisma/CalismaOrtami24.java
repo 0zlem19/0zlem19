f4c1b0f22e1d396e33ec425e4c42421e0a7056b3
Özlem Çetinkaya <21010310061@ogrenci.bartin.edu.tr>
2021-12-18 00:55:47 +0300

    Create CalismaOrtami24.java

---
+++ Calisma/CalismaOrtami24.java
@@ -0,0 +1,12 @@
+
+public class CalismaOrtami2 {
+
+	public static void main(String[] args) {
+		
+		//-32768 ile 32767 arasýnda deðerler alýr. 2byte
+		
+		short sayi= 25550;
+		
+		System.out.println(sayi);
+	}
+}
