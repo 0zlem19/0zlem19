07b871020bab3e13db01f8f16cd626cbf96bf401
Özlem Çetinkaya <21010310061@ogrenci.bartin.edu.tr>
2022-03-17 15:46:01 +0300

    BaharDonemi
    Week3

---
+++ BaharDonemi/PointMain3.java
@@ -0,0 +1,12 @@
+
+public class PointMain3 {
+
+	public static void main(String[] args) {
+
+		Point3 pnt = new Point3(3, 4);
+
+		System.out.println("point = (" + pnt.x + " , " + pnt.y + ")");
+		System.out.println(pnt.DistanceFromOrigin());
+	}
+
+}
